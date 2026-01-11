8faa8f1637887a34d8275772b70f275d094e327b
Karan Salunkhe <karansalunkhe180@gmail.com>
2026-01-11 12:27:39 +0530

    Best_Time_To_Buy_Sell_Stock

---
+++ 121_Best_Time_To_Buy_Sell_Stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int mini = prices[0];
+        int n = prices.size();
+
+        for(int i = 1; i < n; i++){
+            int cost = prices[i] - mini;
+            profit = max(profit, cost);
+            mini = min(mini, prices[i]);
+        }
+
+        return profit;
+    }
+};
+// {N, 1}
