5c11b53a6d86c4d64fec73707883471e6ab116fe
Karan Salunkhe <karansalunkhe180@gmail.com>
2026-07-01 19:40:36 +0530

    Create 2022_Convert_1D_Array_Into_2D_Array.cpp

---
+++ 2022_Convert_1D_Array_Into_2D_Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        
+        if(original.size() != m * n){
+            return {};
+        }
+        vector<vector<int>> ans;
+
+        int k = 0;
+        for(int i = 0; i < m; i++){
+            vector<int> row;
+            for(int j = 0; j < n; j++){
+                row.push_back(original[k++]);
+            }
+            ans.push_back(row);
+        }
+
+        return ans;
+    }
+};
