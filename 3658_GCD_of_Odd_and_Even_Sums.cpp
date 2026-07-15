6db87d195587b7390feecca31487da5320512c6f
Karan Salunkhe <karansalunkhe180@gmail.com>
2026-07-15 20:26:46 +0530

    Add function to compute GCD of odd and even sums
    Implement gcdOfOddEvenSums function to calculate GCD of sums of odd and even numbers up to n.

---
+++ 3658_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+
+        int sumEven = 0;
+        int sumOdd = 0;
+        
+        for(int i = 1; i <= n * 2; i++){
+            if(i % 2 == 0){
+                sumEven += i;
+            }
+            else{
+                sumOdd += i;
+            }
+        }
+
+        return gcd(sumOdd, sumEven);
+    }
+};
