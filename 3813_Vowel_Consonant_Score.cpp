704447115e44f1538eca7a67609397218b1cac29
Karan Salunkhe <karansalunkhe180@gmail.com>
2026-07-24 22:50:45 +0530

    Create 3813_Vowel_Consonant_Score.cpp

---
+++ 3813_Vowel_Consonant_Score.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int vowelConsonantScore(string s) {
+        int v = 0, c = 0;
+
+        for(char ch : s){
+
+            if(isdigit(ch)) continue;
+            if(ch == ' ') continue;
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                v++;
+            else
+                c++;
+
+        }
+
+        if(c == 0) return 0;
+
+        return floor(v / c);
+
+
+    }
+};
