cd3c4094674fe27d3a7a75437ebc281019705216
Karan Salunkhe <karansalunkhe180@gmail.com>
2026-07-28 23:36:03 +0530

    Create 387_First_Unique_Character_in_a_String.cpp

---
+++ 387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> freq(26, 0);
+
+        for(char ch : s){
+            freq[ch - 'a']++;
+        }
+
+        for(int i = 0; i < s.size(); i++){
+            char ch = s[i];
+            if(freq[ch - 'a'] == 1) 
+                return i;
+        }
+
+        return -1;
+    }
+};
